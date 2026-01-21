b9d8b6cbf7d8bc9ea45f2fe3c55a53427a4303fb
maplecitymadman <isaac.sanchezhawkins@gmail.com>
2026-01-21 00:56:57 -0500

    fix: initialize dynamic fake client correctly in tests to prevent panic

--- internal/k8s/policy_discovery_test.go
+++ internal/k8s/policy_discovery_test.go
@@ -4,20 +4,16 @@ import (
 	"context"
 	"testing"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	dynamicfake "k8s.io/client-go/dynamic/fake"
 	"k8s.io/client-go/kubernetes/fake"
 )
 
 func TestGetTopologyWithPolicies(t *testing.T) {
 	scheme := runtime.NewScheme()
-	dynamicClient := dynamicfake.NewSimpleDynamicClient(scheme)
 
 	// Create a Cilium policy
-	ciliumGVR := schema.GroupVersionResource{Group: "cilium.io", Version: "v2", Resource: "ciliumnetworkpolicies"}
 	ciliumPolicy := &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"apiVersion": "cilium.io/v2",
@@ -29,10 +25,8 @@ func TestGetTopologyWithPolicies(t *testing.T) {
 			"spec": map[string]interface{}{},
 		},
 	}
-	dynamicClient.Resource(ciliumGVR).Namespace("default").Create(context.Background(), ciliumPolicy, metav1.CreateOptions{})
 
 	// Create a Kyverno policy
-	kyvernoGVR := schema.GroupVersionResource{Group: "kyverno.io", Version: "v1", Resource: "clusterpolicies"}
 	kyvernoPolicy := &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"apiVersion": "kyverno.io/v1",
@@ -43,7 +37,26 @@ func TestGetTopologyWithPolicies(t *testing.T) {
 			"spec": map[string]interface{}{},
 		},
 	}
-	dynamicClient.Resource(kyvernoGVR).Create(context.Background(), kyvernoPolicy, metav1.CreateOptions{})
+
+	// Create ArgoCD Application (to prevent drift detector panic)
+	argoApp := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "argoproj.io/v1alpha1",
+			"kind":       "Application",
+			"metadata": map[string]interface{}{
+				"name":      "test-app",
+				"namespace": "argocd",
+			},
+			"status": map[string]interface{}{
+				"sync": map[string]interface{}{
+					"status": "Synced",
+				},
+			},
+		},
+	}
+
+	// Initialize dynamic client WITH the objects
+	dynamicClient := dynamicfake.NewSimpleDynamicClient(scheme, ciliumPolicy, kyvernoPolicy, argoApp)
 
 	client := &Client{
 		clientset:     fake.NewSimpleClientset(),
@@ -63,10 +76,8 @@ func TestGetTopologyWithPolicies(t *testing.T) {
 
 func TestGetTopologyWithIstio(t *testing.T) {
 	scheme := runtime.NewScheme()
-	dynamicClient := dynamicfake.NewSimpleDynamicClient(scheme)
 
 	// Create Istio Gateway
-	gatewayGVR := schema.GroupVersionResource{Group: "networking.istio.io", Version: "v1beta1", Resource: "gateways"}
 	gateway := &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"apiVersion": "networking.istio.io/v1beta1",
@@ -80,10 +91,8 @@ func TestGetTopologyWithIstio(t *testing.T) {
 			},
 		},
 	}
-	dynamicClient.Resource(gatewayGVR).Namespace("default").Create(context.Background(), gateway, metav1.CreateOptions{})
 
 	// Create Istio VirtualService
-	vsGVR := schema.GroupVersionResource{Group: "networking.istio.io", Version: "v1beta1", Resource: "virtualservices"}
 	vs := &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"apiVersion": "networking.istio.io/v1beta1",
@@ -109,7 +118,21 @@ func TestGetTopologyWithIstio(t *testing.T) {
 			},
 		},
 	}
-	dynamicClient.Resource(vsGVR).Namespace("default").Create(context.Background(), vs, metav1.CreateOptions{})
+
+	// Create ArgoCD Application (to prevent drift detector panic)
+	argoApp := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "argoproj.io/v1alpha1",
+			"kind":       "Application",
+			"metadata": map[string]interface{}{
+				"name":      "test-app",
+				"namespace": "argocd",
+			},
+		},
+	}
+
+	// Initialize dynamic client WITH the objects
+	dynamicClient := dynamicfake.NewSimpleDynamicClient(scheme, gateway, vs, argoApp)
 
 	client := &Client{
 		clientset:     fake.NewSimpleClientset(),
